a90f08a99872f267e5b3251705b1c3fdd708762e
Matti Brandt <raxiam@gmail.com>
2020-11-10 23:06:50 +0100

    Updating quest Lazy Lisa

--- Alpha/atherysquests/dialogs/lisa-dialog.groovy
+++ Alpha/atherysquests/dialogs/lisa-dialog.groovy
@@ -1,3 +1,18 @@
-onDialogRegistration({event -> 
+onDialogRegistration({ event -> 
     
-}as Consumer)
+    def id = "";
+    def root = dialogNode(0)
+        .player("")
+        .npc([
+            "",
+            ""
+        ])
+        .build();
+    
+    def tree = dialogTree(id, root);
+
+    if(tree != null){
+        info("Dialog " + id + " successfully created");
+        event.getManager().registerDialog(tree);
+    }
+} as Consumer);
